d18269248c646d2e163d6421276db0d8d5aab744
OscarRapale <you@example.com>
2024-04-21 15:15:49 -0400

    Resolving merge issue

--- shell.c
+++ shell.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * main - Entry point and main loop of shell
+ * main - Entry point of shell
  *
  * Return: 0 on success
  */
@@ -10,44 +10,13 @@ int main(void)
 {
 	if (isatty(STDIN_FILENO) == 1)
 	{
-<<<<<<< HEAD
 		shell_interactive();
 	}
 	else
 	{
 		shell_no_interactive();
-=======
-		if (isatty(STDIN_FILENO))
-			printf("\nuser@simple_shell$ ");
-
-		buffer = read_input();
-		array = tokenize_input(buffer);
-
-		if (array == NULL)
-		{
-			free(buffer);
-			continue;
-		}
-
-		if (strcmp(array[0], "exit") == 0)
-		{
-			free(buffer);
-			free(array);
-			exit(0);
-		}
-		if (strcmp(array[0], "env") == 0)
-		{
-			env(array);
-			continue;
-		}
-		path = get_file_path(array[0]);
-
-		create_process(path, array);
-
-		free(buffer);
-		free(array);
->>>>>>> main
 	}
+
 	return (0);
 }
 
