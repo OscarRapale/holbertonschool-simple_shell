1450400ee54852cdefb39bc2fb1b931b963225b0
OscarRapale <you@example.com>
2024-04-23 23:44:40 -0400

    Testing exit status

--- shell.h
+++ shell.h
@@ -10,6 +10,7 @@
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdbool.h>
 
 extern char **environ;
 
@@ -19,17 +20,16 @@ extern char **environ;
 int leading_slash_in_path(char const *str);
 char *get_file_path(char *file_name);
 char *find_executable_path(char *path, char *file_name);
-int execute_command(char *input, char *argv[], char **env);
+void execute_command(char *input, char *argv[], char **env);
 char *read_input(void);
 int tokenize_input(char *input, char *args[]);
 char *handle_symbols(char *input);
 
 /* Built-in functions */
-int builtin_commands(char **args, int num_args,
-    char *input, char **env);
+int builtin_commands(char **args, int num_args, char **env);
 void print_env(char **env);
 void handle_cd(char **args, int num_args);
 void handle_exit(char *input, int exit_status);
-int shell_exit(char **args, char *input);
+void shell_exit(int status, bool is_exit_command);
 
 #endif /* SHELL_H */
